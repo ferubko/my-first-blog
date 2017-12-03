ea91fd68bd132f69d37d19fbbacfd26cafd8aff7
stepanferubko <stepan.ferubko@flexshore.com>
2017-12-03 15:04:58 +0200

    changed model

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,8 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    # author = models.ForeignKey('auth.User')
+    author = models.TextField()
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
