3f4adafd4a5e0bc5752ed7461825d7d14857f958
stepanferubko <stepan.ferubko@flexshore.com>
2017-12-03 20:22:42 +0200

    changed style

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
-from django.db import models
-from django.utils import timezone
 # from twisted.mail.smtp import User
 from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model):
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
+from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from django.shortcuts import redirect
-from .models import Post
+
 from .forms import PostForm
-from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 
 def post_list(request):
